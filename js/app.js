fe89f3d3339e98aee9cf0046f742bdfee9b87493
Bao Cong Pham <BaoPham92@yahoo.com>
2017-06-22 06:31:28 -0400

    just got rails downloaded

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
-})();
+  angular
+  	.module("wdinstagram", ['ui.router', 'ngResource'])
+  		.config(['$stateProvider', '$locationProvider', RouterFunction])
+  			.factory('InstaFactory', ['$resource', function($resource){
+				return $resource('http://localhost3000/photos/:id')
+					}])
+
+  			.controller('indexController', ['InstaFactory',function(InstaFactory){
+  				this.photo = instaFactory.query()
+  						this.create = function(){}
+  				}])
+  			.controller('newController', ['InstaFactory', '$stateParams',function(InstaFactory){
+  				this.photo = instaFactory.query()
+  					this.photo.$save().then(function(){
+  						$state.go("index")
+  					})
+  			}])
+  	.controller('showController', ['InstaFactory', '$stateParams',function(InstaFactory,$stateParams){
+  		this.photo = instaFactory.query()
+  			this.create = function(){}
+  	}])
+})
+
+
+function RouterFunction($stateProvider){
+	$stateProvider
+		$state('index',{
+			url: '/index',
+				templateUrl: 'js/ng-views/index.html',
+					controller: 'indexController',
+						controllerAs: 'vm',
+	})
+		.state('new',{
+			url: '/new',
+				templateUrl: 'js/ng-views/new.html',
+					controller: 'newController',
+						controllerAs: 'vm',
+	})
+	.state('show',{
+			url: '/show/:id',
+				templateUrl: 'js/ng-views/show.html',
+					controller: 'showController',
+						controllerAs: 'vm',
+	})
+
+}
